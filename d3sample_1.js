dc76c3da1348b4c9b2b8bcfe22ebc937e4f2bed0
Kiran Danduprolu <gitk@danduprolu.com>
2012-04-13 17:29:28 +0530

    Horizontal and Vertical Bar chart demo code added.

--- d3sample_1.js
+++ d3sample_1.js
@@ -19,8 +19,26 @@ init = function() {
 init = function() {
 	test = new cv.graph();
 	test.init(cv.constants.defaultGraphdef);
-	test.xAxis.tickSubdivide(5); test.horGrid.call(test.xAxis);
+	//test.xAxis.tickSubdivide(5); test.horGrid.call(test.xAxis);
 	//test.remove();
 	//test.removeHorAxis();
 	//test.removeVerAxis();
+	//test.removeHorGrid();
+	testData = [ 40, 20, 80, 30, 100, 30, 10, 50, 20, 40, 60, 90 ];
+	/*xdscale = d3.scale.ordinal().domain(testData).rangeRoundBands([0, test.dimension.width - test.margin.left - test.margin.right], .2);
+	test.bar = test.panel.selectAll("g.bar").data(testData).enter().append("rect")
+				.attr("height", function (d) { return test.dimension.height - test.margin.top - test.margin.bottom - test.yScale(d);})
+				.attr("width", xdscale.rangeBand())
+				.attr("y", function (d) { return test.yScale(d);})
+				.attr("x", function (d) { return xdscale(d);})
+				.style("fill", "#eee").style("stroke","#111");*/
+
+	/*ydscale = d3.scale.ordinal().domain(testData).rangeRoundBands([0, test.dimension.height - test.margin.top - test.margin.bottom], .2);
+	test.bar = test.panel.selectAll("g.bar").data(testData).enter().append("rect")
+				.attr("height", ydscale.rangeBand())
+				.attr("width", function (d) { return test.xScale(d);})
+				.attr("x", function (d) {return 0;})
+				.attr("y", function (d) {return ydscale(d);})
+				.style("fill", "#eee").style("stroke","#111");
+				*/
 }
