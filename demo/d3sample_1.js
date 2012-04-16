c1d1d98f4a18657f687ac1df27b5fec055fba391
Kiran Danduprolu <gitk@danduprolu.com>
2012-04-16 17:47:20 +0530

    Demo changes with respect to changes made today

--- demo/d3sample_1.js
+++ demo/d3sample_1.js
@@ -18,13 +18,10 @@ init = function() {
 
 init = function() {
 	test = new cv.graph();
-	test.init(cv.constants.defaultGraphdef);
-	//test.xAxis.tickSubdivide(5); test.horGrid.call(test.xAxis);
-	//test.remove();
-	//test.removeHorAxis();
-	//test.removeVerAxis();
-	//test.removeHorGrid();
-	testData = [ 60, 70, 80, 90 ];
+	graphdef = cv.constants.defaultGraphdef;
+
+	test.init(graphdef);
+
 	/*xdscale = d3.scale.ordinal().domain(testData).rangeRoundBands([0, test.dimension.width - test.margin.left - test.margin.right], .2);
 	test.bar = test.panel.selectAll("g.bar").data(testData).enter().append("rect")
 				.attr("height", function (d) { return test.dimension.height - test.margin.top - test.margin.bottom - test.yScale(d);})
@@ -33,25 +30,16 @@ init = function() {
 				.attr("x", function (d) { return xdscale(d);})
 				.style("fill", "#eee").style("stroke","#111");*/
 
-	test.xScale 	= d3.scale.linear().domain([0,100]).range([0, test.dimension.width]);
-	ydscale = d3.scale.ordinal().domain(testData).rangeRoundBands([0, test.dimension.height], .4);
-	test.bar = test.panel.selectAll("g.bar").data(testData).enter().append("rect")
-				.attr("height", ydscale.rangeBand())
-				.attr("width", function (d) { return test.xScale(d);})
+	test.setHorAxis();
+	test.setVerAxis();
+	test.axes.ver.scale.domain(graphdef.data);
+
+	test.bar = test.panel.selectAll("g.bar").data(graphdef.data).enter().append("rect")
+				.attr("height", test.axes.ver.scale.rangeBand())
+				.attr("width", function (d) { return test.axes.hor.scale(d);})
 				.attr("x", function (d) {return 0;})
-				.attr("y", function (d) {return ydscale(d);})
-				.style("fill", "#eee").style("stroke","#111")
-		//		.on("mouseover", function() { style('fill', '#ddd');});
+				.attr("y", function (d) {return test.axes.ver.scale(d);})
+				.style("fill", "#eee").style("stroke","#111");
 
-	
-	test.xAxis 		= d3.svg.axis().scale(test.xScale).ticks(2).tickSize(test.dimension.height, 10).tickSubdivide(4).orient("bottom");
-	test.horAxis 	= test.panel.append("g").attr("class", "x axis")	      
-					      .call(test.xAxis);
-
-	test.yScale		= d3.scale.linear().domain([0,100]).range([test.dimension.height, 0]);
-	test.yAxis 		= d3.svg.axis().scale(test.yScale).ticks(4).orient("left").tickSize(6, -width);
-/*	test.verAxis 	= test.panel.append("g").attr("class", "y axis")	
-					      .call(test.yAxis); */
-	test.verAxis = test.panel.append('g').attr("class", "y axis").append('line').attr('y1', test.dimension.height).attr('y2', test.dimension.height).attr('x1','100%');
-
+	console.log(test);
 }
