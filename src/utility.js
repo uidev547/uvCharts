82fb275598fadf1257f103654291ee09a53ee374
Kiran Danduprolu <gitk@danduprolu.com>
2012-04-16 17:43:13 +0530

    Added max function for multibar and normal series

--- src/utility.js
+++ src/utility.js
@@ -9,3 +9,13 @@ cv.utility = {};
 cv.utility.getPascalCasedName = function (name) {
 	return name.substring(0,1).toUpperCase() + name.substring(1);
 };
+
+cv.utility.max = function (graphdef) {
+	if (graphdef.data) {
+		return d3.max(graphdef.data);
+	} else {
+		//TODO
+		var maxVals = [];
+		return 0;
+	}
+}
