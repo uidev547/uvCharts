08c67fc4ec560ac37822aac3fd063966cbc82454
kd <gitk@danduprolu.com>
2012-04-18 14:56:10 +0530

    cleaned sample.js to single line of code

--- demo/d3sample_1.js
+++ demo/d3sample_1.js
@@ -1,30 +1,3 @@
-/*
-init = function() { 
-	var data = [4, 8, 15, 16, 23, 42];
-
-	var chart = d3.select("body").append("svg")
-					.attr("class", "chart"). attr("width", 420)
-					.attr("height", 20*data.length);
-
-	var x = d3.scale.linear().domain([0, d3.max(data)]).range([0, 420]);
-
-	chart.selectAll("rect").data(data)
-		.enter().append("rect")
-		.attr("y", function(d,i) { return i*20;})
-		.attr("width", x)
-		.attr("height", 20);
-};
-*/
-
-/*xdscale = d3.scale.ordinal().domain(testData).rangeRoundBands([0, test.dimension.width - test.margin.left - test.margin.right], .2);
-	test.bar = test.panel.selectAll("g.bar").data(testData).enter().append("rect")
-				.attr("height", function (d) { return test.dimension.height - test.margin.top - test.margin.bottom - test.yScale(d);})
-				.attr("width", xdscale.rangeBand())
-				.attr("y", function (d) { return test.yScale(d);})
-				.attr("x", function (d) { return xdscale(d);})
-				.style("fill", "#eee").style("stroke","#111");*/
-//	test.axes.ver.scale.domain(graphdef.data.map( function(d) { return d.name;}));
-
 init = function() {
 	test = new cv.bargraph(cv.constants.defaultGraphdef);
 }
