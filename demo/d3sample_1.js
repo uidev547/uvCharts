360948df6173acca751369a3a0ec91d776db58f0
Kiran Danduprolu <gitk@danduprolu.com>
2012-04-19 16:33:44 +0530

    Demo changes to include line graph

--- demo/d3sample_1.js
+++ demo/d3sample_1.js
@@ -2,4 +2,5 @@ init = function() {
 	barTest = new cv.bargraph(cv.constants.defaultGraphdef);
 	stepUpBarTest = new cv.stepup_bargraph(cv.constants.defaultGraphdef);
 	stackedBarTest = new cv.stacked_bargraph(cv.constants.defaultGraphdef);
+	lineTest = new cv.linegraph(cv.constants.defaultGraphdef);
 }
