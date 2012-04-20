9f7ff5593b748ed7223d2ad58d32d2eb71818679
kd <gitk@danduprolu.com>
2012-04-21 03:09:48 +0530

    Donut and Pie graphs added

---
+++ src/donut.js
@@ -0,0 +1,36 @@
+cv.donutgraph = function (graphdef) {
+	cv.graph.apply(this, [graphdef]);
+	this.init(graphdef);
+
+	this.outerRadius = Math.min(this.dimension.height, this.dimension.width)/2;
+	this.innerRadius = this.outerRadius * 0.6;
+
+	this.category = graphdef.categories[0];
+	this.data = r3.util.getCategoryData(this.graphdef, [this.category]);
+
+	color = d3.scale.category20();
+
+	this.donutfunc = d3.layout.pie();
+	this.arcfunc = d3.svg.arc().innerRadius(this.innerRadius).outerRadius(this.outerRadius);
+
+	this.panel.data(this.data);
+
+	this.arcs = this.panel.selectAll('g.arc')
+					.data(this.donutfunc).enter()
+					.append('g').attr('class','arc')
+					.attr('transform', 'translate(' + this.outerRadius + ',' + this.outerRadius + ')');
+
+	this.arcs.append("path")
+	    .attr("fill", function(d, i) { return color(i); })
+	    .attr("d", this.arcfunc);
+
+	var arc = this.arcfunc;
+	this.arcs.append("text")
+	    .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+	    .attr("dy", ".35em")
+	    .attr("text-anchor", "middle")
+	    .attr("display", function(d) { return d.value > .15 ? null : "none"; })
+	    .text(function(d, i) { return d.value; });
+};
+
+cv.donutgraph.prototype = cv.extend(cv.graph);
