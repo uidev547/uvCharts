59c5051f77e051bb7e6a395e433dfd3c0a39794f
Kiran Danduprolu <gitk@danduprolu.com>
2012-04-24 15:50:53 +0530

    More test cases added for transposed datasets

--- test/d3sample_1.js
+++ test/d3sample_1.js
@@ -10,17 +10,57 @@ init = function() {
 	r3.util.transposeData(r3.constants.defaultHorGraphdef);
 	
 	stepUpBarHorTest = new r3.stepup_bargraph(r3.constants.defaultHorGraphdef);
+	r3.util.transposeData(r3.constants.defaultHorGraphdef);
+	tstepUpBarHorTest = new r3.stepup_bargraph(r3.constants.defaultHorGraphdef);
 	stepUpBarVerTest = new r3.stepup_bargraph(r3.constants.defaultVerGraphdef);
+	r3.util.transposeData(r3.constants.defaultVerGraphdef);
+	tstepUpBarVerTest = new r3.stepup_bargraph(r3.constants.defaultVerGraphdef);
+	
+	r3.util.transposeData(r3.constants.defaultVerGraphdef);	
+	r3.util.transposeData(r3.constants.defaultHorGraphdef);
+	
 	stackedBarHorTest = new r3.stacked_bargraph(r3.constants.defaultHorGraphdef);
-	stackedBarVerTest = new r3.stacked_bargraph(r3.constants.defaultVerGraphdef);	
+	r3.util.transposeData(r3.constants.defaultHorGraphdef);
+	tstackedBarHorTest = new r3.stacked_bargraph(r3.constants.defaultHorGraphdef);
+	stackedBarVerTest = new r3.stacked_bargraph(r3.constants.defaultVerGraphdef);
+	r3.util.transposeData(r3.constants.defaultVerGraphdef);
+	tstackedBarVerTest = new r3.stacked_bargraph(r3.constants.defaultVerGraphdef);
+
+	r3.util.transposeData(r3.constants.defaultVerGraphdef);	
+	r3.util.transposeData(r3.constants.defaultHorGraphdef);
+	
 	lineHorTest = new r3.linegraph(r3.constants.defaultHorGraphdef);
+	r3.util.transposeData(r3.constants.defaultHorGraphdef);
+	tlineHorTest = new r3.linegraph(r3.constants.defaultHorGraphdef);
 	lineVerTest = new r3.linegraph(r3.constants.defaultVerGraphdef);
+	r3.util.transposeData(r3.constants.defaultVerGraphdef);
+	tlineVerTest = new r3.linegraph(r3.constants.defaultVerGraphdef);
+
+	r3.util.transposeData(r3.constants.defaultVerGraphdef);	
+	r3.util.transposeData(r3.constants.defaultHorGraphdef);
+	
 	areaHorTest = new r3.areagraph(r3.constants.defaultHorGraphdef);
+	r3.util.transposeData(r3.constants.defaultHorGraphdef);
+	tareaHorTest = new r3.areagraph(r3.constants.defaultHorGraphdef);
 	areaVerTest = new r3.areagraph(r3.constants.defaultVerGraphdef);
+	r3.util.transposeData(r3.constants.defaultVerGraphdef);
+	tareaVerTest = new r3.areagraph(r3.constants.defaultVerGraphdef);
+
+	r3.util.transposeData(r3.constants.defaultVerGraphdef);	
+	r3.util.transposeData(r3.constants.defaultHorGraphdef);
+
 	stackareaHorTest = new r3.stacked_areagraph(r3.constants.defaultHorGraphdef);
 	stackareaVerTest = new r3.stacked_areagraph(r3.constants.defaultVerGraphdef);
 	donutTest = new r3.donutgraph(r3.constants.defaultVerGraphdef);
 	pieTest = new r3.piegraph(r3.constants.defaultVerGraphdef);
+	
 	tableHorTest = new r3.tablegraph(r3.constants.defaultHorGraphdef);
+	r3.util.transposeData(r3.constants.defaultHorGraphdef);
+	ttableHorTest = new r3.tablegraph(r3.constants.defaultHorGraphdef);
 	tableVerTest = new r3.tablegraph(r3.constants.defaultVerGraphdef);
+	r3.util.transposeData(r3.constants.defaultVerGraphdef);
+	tableVerTest = new r3.tablegraph(r3.constants.defaultVerGraphdef);
+	
+	r3.util.transposeData(r3.constants.defaultVerGraphdef);	
+	r3.util.transposeData(r3.constants.defaultHorGraphdef);
 };
