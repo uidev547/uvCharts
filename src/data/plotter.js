16a3d8c25706859b979b2e8941330d0907f5e0f5
Kiran Danduprolu <gitk@danduprolu.com>
2012-05-01 15:14:34 +0530

    Plotter for data to help visualize data in graphs

---
+++ src/data/plotter.js
@@ -0,0 +1,49 @@
+r3.data.prototype.graph = function (graphtype) {
+	if( this.measures.length > 0 && this.category ) {
+		var path = [];
+		this.plot(this.dataset, path, graphtype);
+	};
+};
+
+r3.data.prototype.plot = function (dataset, path, graphtype) {
+	
+	if (dataset && dataset.data && dataset.data.length > 0) {
+		var graphdef = { 'categories' : [], 'dataset' : undefined }
+		
+		dataset.plotData = {};
+		for (var category in this.KeySet[this.category] ) {
+			graphdef.categories.push(category);
+			dataset.plotData[category] = [];
+			for (var i = 0, length = this.measures.length; i<length; i = i + 1) {
+				dataset.plotData[category].push({ 'name' : this.measures[i], 'value' : +0});
+			}
+		}
+		
+		for (var i = 0, length = dataset.data.length; i<length; i = i + 1) {
+			var category = dataset.data[i][this.category];
+			for (var j =0, jlength = dataset.plotData[category].length; j < jlength; j = j + 1) {
+				var measure = dataset.plotData[category][j]['name'];
+				dataset.plotData[category][j].value += (+dataset.data[i][measure]);
+			}
+		}
+		
+		graphdef.dataset = dataset.plotData;
+		
+		graphdef.stepup = false; graphdef.orientation = 'hor'; graphdef.pos = 'body';
+		r3.util.transposeData(graphdef);
+		d3.select('#r3_div').append('div').text(path.join(' || ')); d3.select('#r3_div').append('hr');
+		var plotgraph = new r3[graphtype](graphdef);
+		
+		delete dataset.plotData;
+		delete graphdef;		
+		return;
+	} else {
+		for (var key in dataset) {
+			path.push(key);
+			this.plot (dataset[key], path, graphtype);
+			path.pop();
+		}
+		
+		return;
+	}
+};
