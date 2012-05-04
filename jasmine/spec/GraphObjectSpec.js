ee63105f6c1ca7e9b9a2d45fbace99d3a0210edf
Kiran Danduprolu <gitk@danduprolu.com>
2012-05-04 17:58:26 +0530

    Few more test cases for r3.graph object added

--- jasmine/spec/GraphObjectSpec.js
+++ jasmine/spec/GraphObjectSpec.js
@@ -12,4 +12,28 @@ describe('GraphObject', function () {
 	
 	var initGraph = new r3.graph();
 	initGraph.init(r3.constants.defaultHorGraphdef);
+	it('should have right properties on initialization', function () {
+		expect(initGraph.width()).toEqual(400);
+		expect(initGraph.height()).toEqual(400);
+		expect(initGraph.top()).toEqual(40);
+		expect(initGraph.bottom()).toEqual(90);
+		expect(initGraph.left()).toEqual(100);
+		expect(initGraph.right()).toEqual(100);
+		expect(initGraph.position()).toEqual(r3.config.meta.position);
+		expect(initGraph.max()).toEqual(150);
+		
+		expect(initGraph.frame).toBeDefined();
+		expect(initGraph.panel).toBeDefined();
+		expect(initGraph.bg).toBeDefined();
+		expect(initGraph.labels).toBeDefined();
+		expect(initGraph.categories).toBeDefined();
+	});
+	
+	var frameLessGraph = new r3.graph();
+	frameLessGraph.init(r3.constants.defaultHorGraphdef);
+	frameLessGraph.remove();
+	it('should be able to remove its own frame', function () {
+		//expect(frameLessGraph.frame).toBeFalsy();
+	});
+	
 });
