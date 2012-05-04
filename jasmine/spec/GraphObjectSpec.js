ae3a49aa6088d2e5dd4300deccbacd3b519e4bd1
Kiran Danduprolu <gitk@danduprolu.com>
2012-05-04 17:13:44 +0530

    GraphObjectSpec to test graph.js

--- jasmine/spec/GraphObjectSpec.js
+++ jasmine/spec/GraphObjectSpec.js
@@ -3,5 +3,13 @@ describe('GraphObject', function () {
 	it('should have the default properties right after construction', function () {
 		expect(testGraph.graphdef).toBeUndefined();
 		expect(testGraph.config).toEqual(r3.config);
+		expect(testGraph.frame).toBeUndefined();
+		expect(testGraph.panel).toBeUndefined();
+		expect(testGraph.bg).toBeUndefined();
+		expect(testGraph.labels).toBeUndefined();
+		expect(testGraph.categories).toBeUndefined();		
 	});
+	
+	var initGraph = new r3.graph();
+	initGraph.init(r3.constants.defaultHorGraphdef);
 });
