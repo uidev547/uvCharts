d78de62473d391adb367a7b70a231f45faedfc83
hashd <gitk@danduprolu.com>
2012-05-05 20:46:52 +0530

    Similar changes in pie.js

--- src/gfx/pie.js
+++ src/gfx/pie.js
@@ -2,56 +2,44 @@ r3.piegraph = function (graphdef) {
 	r3.graph.apply(this, [graphdef]);
 	this.init(graphdef);
 
-	this.outerRadius = Math.min(this.height(), this.width()) * 2 / 5;
-	this.innerRadius = 0;
+	this.radius = Math.min(this.height(), this.width()) * 2 / 5;
 	this.center = {
 		x : this.width() / 2,
 		y : this.height() / 2
 	};
-
 	this.category = graphdef.categories[0];
-	this.data = r3.util.getCategoryData(this.graphdef, [this.category]);
 
-	this.layout = d3.layout.pie();
-	this.arcfunc = d3.svg.arc().innerRadius(this.innerRadius).outerRadius(this.outerRadius);
-	this.panel.data(this.data);
+	var data = r3.util.getCategoryData(this.graphdef, [this.category]),
+		arcfunc = d3.svg.arc().innerRadius(0).outerRadius(this.radius),
+		config = this.config,
+		center = this.center,
+		layout = d3.layout.pie();
 
-	var config = this.config,
-		arc = this.arcfunc,
-		center = this.center;
-
+	this.panel.data(data);
 	this.arcs = this.panel.selectAll('g.arc')
-					.data(this.layout).enter()
-					.append('g').attr('class', 'arc')
+					.data(layout).enter()
+					.append('g').attr('class', 'r3_arc' + this.category)
 					.attr('transform', 'translate(' + this.center.x + ',' + this.center.y + ')');
 
-	this.arcpath = this.arcs.append('path')
-	    .attr('fill', function (d, i) { return r3.util.getColorBand(config, i); }).attr('d', this.arcfunc)
-		.on('mouseover', function (d, i) {
-			var dev = {
-				x : arc.centroid(d)[0] / 4,
-				y : arc.centroid(d)[1] / 4
-			};
-			d3.select(this.parentNode)
-				.attr('transform', 'translate(' + (center.x + dev.x) + ',' + (center.y + dev.y) + ')');
-		})
-		.on('mouseout', function () {
-			d3.select(this.parentNode).attr('transform', 'translate(' + center.x + ',' + center.y + ')');
-		});
+	this.arcs.append('path')
+	    .attr('d', arcfunc)
+	    .style('fill', function (d, i) { return r3.util.getColorBand(config, i); })
+	    .style('stroke', this.config.pie.strokecolor)
+	    .style('stroke-width', this.config.pie.strokewidth)
+		.on('mouseover', r3.effects.pie.mouseover(center, arcfunc))
+		.on('mouseout', r3.effects.pie.mouseout(center));
 
 	this.arcs.append('text')
-	    .attr('transform', function (d) { return 'translate(' + arc.centroid(d) + ')'; })
+	    .attr('transform', function (d) { return 'translate(' + arcfunc.centroid(d) + ')'; })
 	    .attr('dy', '.35em')
 	    .attr('text-anchor', 'middle')
-	    .attr('display', function (d) { return d.value > 15 ? null : 'none'; })
+//	    .attr('display', function (d) { return d.value > 15 ? null : 'none'; })
 	    .style('fill', this.config.pie.fontfill)
 	    .style('font-family', this.config.pie.fontfamily)
 	    .style('font-size', this.config.pie.fontsize)
 	    .style('font-weight', this.config.pie.fontweight)
 	    .style('font-variant', this.config.pie.fontvariant)
-	    .text(function (d, i) { return d.value; });
-
-	console.log(this);
+	    .text(function (d) { return d.value; });
 };
 
 r3.piegraph.prototype = r3.util.extend(r3.graph);
