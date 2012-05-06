8e959ca3a1e4b25da9ec923e0b0482e09177740f
kd <gitk@danduprolu.com>
2012-05-07 02:17:54 +0530

    Refactored code for percent and stacked area graph

--- src/gfx/percent_areagraph.js
+++ src/gfx/percent_areagraph.js
@@ -15,16 +15,16 @@ r3.percent_areagraph = function (graphdef) {
 		domainData = this.labels, 
 		categories = this.categories;
 
-	this.axes[this.graphdef.orientation === 'hor' ? 'ver' : 'hor'].scale.domain(domainData.map(function (d) { return d; }));
+	this.axes[this.graphdef.orientation === 'hor' ? 'ver' : 'hor'].scale.domain(domainData);
 	this.areagroup = this.panel.selectAll('g.areagroup').data(stacklayout).enter().append('g').attr('class', 'areagroup');
-	this['draw' + r3.util.getPascalCasedName(this.graphdef.orientation) + 'StackArea']();
+	this['draw' + this.graphdef.orientation + 'StackArea']();
 
 	this.finalize();
 };
 
 r3.percent_areagraph.prototype = r3.util.extend(r3.graph);
 
-r3.percent_areagraph.prototype.drawHorStackArea = function () {
+r3.percent_areagraph.prototype.drawhorStackArea = function () {
 	var axes = this.axes,
 		categories = this.categories,
 		config = this.config,
@@ -32,27 +32,29 @@ r3.percent_areagraph.prototype.drawHorStackArea = function () {
 	
 	axes.ver.scale.rangePoints([0, this.height()]);
 
-	this.areapath = this.areagroup.append('path')
-					    .attr('class', function (d, i) { return 'areapath_' + categories[i]; })
-					    .style('fill', function (d, i) { return r3.util.getColorBand(config, i); })
-					    .attr('d', d3.svg.area()
-						    .y(function (d) { return axes.ver.scale(d.x) + axes.ver.scale.rangeBand() / 2; })
-						    .x0(function (d, i) { return axes.hor.scale(r3.util.getPercentage(d.y0, sumMap[i])); })
-						    .x1(function (d, i) { return axes.hor.scale(r3.util.getPercentage(d.y0 + d.y, sumMap[i])); })
-						)
-						.on('mouseover', function () {d3.select(this).style('fill', r3.config.effects.hovercolor); })
-						.on('mouseout',  function (d, i) {d3.select(this).style('fill', r3.util.getColorBand(config, i)); });
+	this.areagroup.append('path')
+	    .attr('class', function (d, i) { return 'area_' + categories[i]; })
+	    .style('fill', function (d, i) { return r3.util.getColorBand(config, i); })
+	    .attr('d', d3.svg.area()
+		    .y(function (d) { return axes.ver.scale(d.x) + axes.ver.scale.rangeBand() / 2; })
+		    .x0(function (d, i) { return axes.hor.scale(r3.util.getPercentage(d.y0, sumMap[i])); })
+		    .x1(function (d, i) { return axes.hor.scale(r3.util.getPercentage(d.y0 + d.y, sumMap[i])); })
+		)
+		.on('mouseover', function () {d3.select(this).style('fill', r3.config.effects.hovercolor); })
+		.on('mouseout',  function (d, i) {d3.select(this).style('fill', r3.util.getColorBand(config, i)); });
 
-	this.linepath = this.areagroup.append('path')
-						.attr('class', function (d, i) { return 'linepath_' + categories[i]; })
-						.style('stroke', 'white').style('fill', 'none').style('stroke-width', 2)
-						.attr('d', d3.svg.line()
-						    .y(function (d) { return axes.ver.scale(d.x) + axes.ver.scale.rangeBand() / 2; })
-						    .x(function (d, i) { return axes.hor.scale(r3.util.getPercentage(d.y0 + d.y, sumMap[i])); })
-						);
+	this.areagroup.append('path')
+		.attr('class', function (d, i) { return 'line_' + categories[i]; })
+		.style('stroke', 'white')
+		.style('fill', 'none')
+		.style('stroke-width', 2)
+		.attr('d', d3.svg.line()
+		    .y(function (d) { return axes.ver.scale(d.x) + axes.ver.scale.rangeBand() / 2; })
+		    .x(function (d, i) { return axes.hor.scale(r3.util.getPercentage(d.y0 + d.y, sumMap[i])); })
+		);
 };
 
-r3.percent_areagraph.prototype.drawVerStackArea = function () {
+r3.percent_areagraph.prototype.drawverStackArea = function () {
 	var axes = this.axes,
 		categories = this.categories,
 		config = this.config,
@@ -61,21 +63,23 @@ r3.percent_areagraph.prototype.drawVerStackArea = function () {
 	axes.hor.scale.rangePoints([0, this.width()]);
 
 	this.areapath = this.areagroup.append('path')
-					    .attr('class', function (d, i) { return 'areapath_' + categories[i]; })
-					    .style('fill', function (d, i) { return r3.util.getColorBand(config, i); })
-					    .attr('d', d3.svg.area()
-						    .x(function (d) { return axes.hor.scale(d.x) + axes.hor.scale.rangeBand() / 2; })
-						    .y0(function (d, i) { return axes.ver.scale(r3.util.getPercentage(d.y0, sumMap[i])); })
-						    .y1(function (d, i) { return axes.ver.scale(r3.util.getPercentage(d.y0 + d.y, sumMap[i])); })
-					    )
-						.on('mouseover', function () {d3.select(this).style('fill', r3.config.effects.hovercolor); })
-						.on('mouseout',  function (d, i) {d3.select(this).style('fill', r3.util.getColorBand(config, i)); });
+	    .attr('class', function (d, i) { return 'area_' + categories[i]; })
+	    .style('fill', function (d, i) { return r3.util.getColorBand(config, i); })
+	    .attr('d', d3.svg.area()
+		    .x(function (d) { return axes.hor.scale(d.x) + axes.hor.scale.rangeBand() / 2; })
+		    .y0(function (d, i) { return axes.ver.scale(r3.util.getPercentage(d.y0, sumMap[i])); })
+		    .y1(function (d, i) { return axes.ver.scale(r3.util.getPercentage(d.y0 + d.y, sumMap[i])); })
+	    )
+		.on('mouseover', function () {d3.select(this).style('fill', r3.config.effects.hovercolor); })
+		.on('mouseout',  function (d, i) {d3.select(this).style('fill', r3.util.getColorBand(config, i)); });
 
-	this.linepath = this.areagroup.append('path')
-					    .attr('class', function (d, i) { return 'linepath_' + categories[i]; })
-					    .style('stroke', 'white').style('fill', 'none').style('stroke-width', 2)
-					    .attr('d', d3.svg.line()
-						    .x(function (d, i) { return axes.hor.scale(d.x) + axes.hor.scale.rangeBand() / 2; })
-						    .y(function (d, i) { return axes.ver.scale(r3.util.getPercentage(d.y0 + d.y, sumMap[i])); })
-					    );
+	this.areagroup.append('path')
+	    .attr('class', function (d, i) { return 'line_' + categories[i]; })
+	    .style('stroke', 'white')
+	    .style('fill', 'none')
+	    .style('stroke-width', 2)
+	    .attr('d', d3.svg.line()
+		    .x(function (d, i) { return axes.hor.scale(d.x) + axes.hor.scale.rangeBand() / 2; })
+		    .y(function (d, i) { return axes.ver.scale(r3.util.getPercentage(d.y0 + d.y, sumMap[i])); })
+	    );
 };
--- src/gfx/stacked_areagraph.js
+++ src/gfx/stacked_areagraph.js
@@ -13,57 +13,67 @@ r3.stacked_areagraph = function (graphdef) {
 
 	this.axes[this.graphdef.orientation === 'hor' ? 'ver' : 'hor'].scale.domain(domainData.map(function (d) { return d; }));
 	this.areagroup = this.panel.selectAll('g.areagroup').data(stacklayout).enter().append('g').attr('class', 'areagroup');
-	this['draw' + r3.util.getPascalCasedName(this.graphdef.orientation) + 'StackArea']();
+	this['draw' + this.graphdef.orientation + 'StackArea']();
 
 	this.finalize();
 };
 
 r3.stacked_areagraph.prototype = r3.util.extend(r3.graph);
 
-r3.stacked_areagraph.prototype.drawHorStackArea = function () {
-	var axes = this.axes, categories = this.categories, config = this.config;
+r3.stacked_areagraph.prototype.drawhorStackArea = function () {
+	var axes = this.axes,
+		categories = this.categories,
+		config = this.config;
+	
 	axes.ver.scale.rangePoints([0, this.height()]);
 
-	this.areapath = this.areagroup.append('path')
-					    .attr('class', function (d, i) { return 'areapath_' + categories[i]; })
-					    .style('fill', function (d, i) { return r3.util.getColorBand(config, i); })
-					    .attr('d', d3.svg.area()
-						    .y(function (d) { return axes.ver.scale(d.x) + axes.ver.scale.rangeBand() / 2; })
-						    .x0(function (d) { return axes.hor.scale(d.y0); })
-						    .x1(function (d) { return axes.hor.scale(d.y0 + d.y); })
-						)
-						.on('mouseover', function () {d3.select(this).style('fill', r3.config.effects.hovercolor); })
-						.on('mouseout',  function (d, i) {d3.select(this).style('fill', r3.util.getColorBand(config, i)); });
+	this.areagroup.append('path')
+	    .attr('class', function (d, i) { return 'area_' + categories[i]; })
+	    .style('fill', function (d, i) { return r3.util.getColorBand(config, i); })
+	    .attr('d', d3.svg.area()
+		    .y(function (d) { return axes.ver.scale(d.x) + axes.ver.scale.rangeBand() / 2; })
+		    .x0(function (d) { return axes.hor.scale(d.y0); })
+		    .x1(function (d) { return axes.hor.scale(d.y0 + d.y); })
+		)
+		.on('mouseover', function () {d3.select(this).style('fill', r3.config.effects.hovercolor); })
+		.on('mouseout',  function (d, i) {d3.select(this).style('fill', r3.util.getColorBand(config, i)); });
 
-	this.linepath = this.areagroup.append('path')
-						.attr('class', function (d, i) { return 'linepath_' + categories[i]; })
-						.style('stroke', 'white').style('fill', 'none').style('stroke-width', 2)
-						.attr('d', d3.svg.line()
-						    .y(function (d) { return axes.ver.scale(d.x) + axes.ver.scale.rangeBand() / 2; })
-						    .x(function (d) { return axes.hor.scale(d.y0 + d.y); })
-						);
+	this.areagroup.append('path')
+		.attr('class', function (d, i) { return 'line_' + categories[i]; })
+		.style('stroke', 'white')
+		.style('fill', 'none')
+		.style('stroke-width', 2)
+		.attr('d', d3.svg.line()
+		    .y(function (d) { return axes.ver.scale(d.x) + axes.ver.scale.rangeBand() / 2; })
+		    .x(function (d) { return axes.hor.scale(d.y0 + d.y); })
+		);
 };
 
-r3.stacked_areagraph.prototype.drawVerStackArea = function () {
-	var axes = this.axes, categories = this.categories, config = this.config;
+r3.stacked_areagraph.prototype.drawverStackArea = function () {
+	var axes = this.axes,
+		categories = this.categories,
+		config = this.config;
+	
 	axes.hor.scale.rangePoints([0, this.width()]);
 
-	this.areapath = this.areagroup.append('path')
-					    .attr('class', function (d, i) { return 'areapath_' + categories[i]; })
-					    .style('fill', function (d, i) { return r3.util.getColorBand(config, i); })
-					    .attr('d', d3.svg.area()
-						    .x(function (d) { return axes.hor.scale(d.x) + axes.hor.scale.rangeBand() / 2; })
-						    .y0(function (d) { return axes.ver.scale(d.y0); })
-						    .y1(function (d) { return axes.ver.scale(d.y0 + d.y); })
-					    )
-						.on('mouseover', function () {d3.select(this).style('fill', r3.config.effects.hovercolor); })
-						.on('mouseout',  function (d, i) {d3.select(this).style('fill', r3.util.getColorBand(config, i)); });
+	this.areagroup.append('path')
+	    .attr('class', function (d, i) { return 'area_' + categories[i]; })
+	    .style('fill', function (d, i) { return r3.util.getColorBand(config, i); })
+	    .attr('d', d3.svg.area()
+		    .x(function (d) { return axes.hor.scale(d.x) + axes.hor.scale.rangeBand() / 2; })
+		    .y0(function (d) { return axes.ver.scale(d.y0); })
+		    .y1(function (d) { return axes.ver.scale(d.y0 + d.y); })
+	    )
+		.on('mouseover', function () {d3.select(this).style('fill', r3.config.effects.hovercolor); })
+		.on('mouseout',  function (d, i) {d3.select(this).style('fill', r3.util.getColorBand(config, i)); });
 
-	this.linepath = this.areagroup.append('path')
-					    .attr('class', function (d, i) { return 'linepath_' + categories[i]; })
-					    .style('stroke', 'white').style('fill', 'none').style('stroke-width', 2)
-					    .attr('d', d3.svg.line()
-						    .x(function (d) { return axes.hor.scale(d.x) + axes.hor.scale.rangeBand() / 2; })
-						    .y(function (d) { return axes.ver.scale(d.y0 + d.y); })
-					    );
+	this.areagroup.append('path')
+	    .attr('class', function (d, i) { return 'line_' + categories[i]; })
+	    .style('stroke', 'white')
+	    .style('fill', 'none')
+	    .style('stroke-width', 2)
+	    .attr('d', d3.svg.line()
+		    .x(function (d) { return axes.hor.scale(d.x) + axes.hor.scale.rangeBand() / 2; })
+		    .y(function (d) { return axes.ver.scale(d.y0 + d.y); })
+	    );
 };
