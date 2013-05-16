c8571084afcd944728fbea459dcade784ebf0008
hashd <gitk@danduprolu.com>
2013-05-16 17:37:57 +0530

    Modifying build properties of the project

--- grunt.js
+++ grunt.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     meta: {
       version: '0.1.0',
-      banner: '/*! r3vis - v<%= meta.version %> - ' +
+      banner: '/*! uvCharts - v<%= meta.version %> - ' +
         '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
         '* http://github.com/hashd \n' +
         '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
@@ -22,22 +22,22 @@ module.exports = function(grunt) {
     
     concat: {
       dist: {
-        src: ['src/gfx/graph.js', 'src/util/utility.js', 'src/util/config.js', 'src/util/constants.js', 'src/util/register.js', 'src/util/effects.js', 'src/util/palette.js', 'src/gfx/area.js', 'src/gfx/bar.js',
+        src: ['src/gfx/graph.js', 'src/util/utility.js', 'src/util/config.js', 'src/util/constants.js', 'src/util/register.js', 'src/util/effects.js', 'src/util/palette.js', 'src/util/test.js', 'src/gfx/area.js', 'src/gfx/bar.js',
               'src/gfx/donut.js', 'src/gfx/line.js', 'src/gfx/percent_area.js', 'src/gfx/percent_bar.js', 'src/gfx/pie.js', 'src/gfx/stacked_area.js', 'src/gfx/stacked_bar.js', 'src/gfx/stepup_bar.js', 
               'src/gfx/table.js', 'src/gfx/tablegraph.js'],
         dest: 'dist/uv.js'
       },
       
       gfx : {
-        src: ['src/gfx/graph.js', 'src/util/utility.js', 'src/util/config.js', 'src/util/constants.js', 'src/util/register.js', 'src/util/effects.js', 'src/util/palette.js', 'src/gfx/area.js', 'src/gfx/bar.js',
+        src: ['src/gfx/graph.js', 'src/util/utility.js', 'src/util/config.js', 'src/util/constants.js', 'src/util/register.js', 'src/util/effects.js', 'src/util/palette.js', 'src/util/test.js', 'src/gfx/area.js', 'src/gfx/bar.js',
               'src/gfx/donut.js', 'src/gfx/line.js', 'src/gfx/percent_area.js', 'src/gfx/percent_bar.js', 'src/gfx/pie.js', 'src/gfx/stacked_area.js', 'src/gfx/stacked_bar.js', 'src/gfx/stepup_bar.js', 
               'src/gfx/table.js', 'src/gfx/tablegraph.js'],
-        dest: 'dist/uv_gfx.js'
+        dest: 'dist/uvcharts.js'
       },
 
       style : {
-      	src : ['src/css/r3.css'],
-				dest : 'dist/r3.css'
+      	src : ['src/css/uv.css'],
+				dest : 'dist/uv.css'
       }
     },
     
@@ -49,7 +49,7 @@ module.exports = function(grunt) {
       
       gfx : {
         src: ['<banner:meta.banner>', '<config:concat.gfx.dest>'],
-        dest: 'dist/uv_gfx.min.js'
+        dest: 'dist/uvcharts.min.js'
       }
     },
     
