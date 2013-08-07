093532a8f8dffae7748bb9ba055a15b3f01a1b1d
Krishnam Raju G <krishnamrajgoud@gmail.com>
2013-08-07 18:25:57 +0530

    banner addition

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,23 +3,27 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-          
+    pkg: grunt.file.readJSON('package.json'),      
+	ban: grunt.file.readJSON('banner.json'),      
     concat: {
+	options : {
+		banner: '/*! \n<%= pkg.name %> <%= pkg.version %><%= ban.copyright %> <%= ban.licence %>*/\n'
+      },
       dist: {
-        src: ['src/gfx/graph.js', 'src/util/utility.js', 'src/util/config.js', 'src/util/constants.js', 'src/util/register.js', 'src/util/effects.js', 'src/util/palette.js', 'src/util/test.js', 'src/gfx/area.js', 'src/gfx/bar.js',
-              'src/gfx/donut.js', 'src/gfx/line.js', 'src/gfx/percent_area.js', 'src/gfx/percent_bar.js', 'src/gfx/pie.js', 'src/gfx/stacked_area.js', 'src/gfx/stacked_bar.js', 'src/gfx/stepup_bar.js', 'src/gfx/waterfall.js', 
+        src: ['src/gfx/graph.js', 'src/util/utility.js', 'src/util/config.js', 'src/util/constants.js', 'src/util/register.js', 'src/util/effects.js', 'src/util/palette.js', 'src/gfx/area.js', 'src/gfx/bar.js',
+              'src/gfx/donut.js', 'src/gfx/line.js', 'src/gfx/percent_area.js', 'src/gfx/percent_bar.js', 'src/gfx/pie.js', 'src/gfx/polar_area.js', 'src/gfx/stacked_area.js', 'src/gfx/stacked_bar.js', 'src/gfx/stepup_bar.js', 'src/gfx/waterfall.js', 
               'src/gfx/table.js', 'src/gfx/tablegraph.js'],
         dest: 'build/uv.js'
       },
       
       gfx : {
-        src: ['src/gfx/graph.js', 'src/util/utility.js', 'src/util/config.js', 'src/util/constants.js', 'src/util/register.js', 'src/util/effects.js', 'src/util/palette.js', 'src/util/test.js', 'src/gfx/area.js', 'src/gfx/bar.js', 'src/gfx/donut.js', 'src/gfx/line.js', 'src/gfx/percent_area.js', 'src/gfx/percent_bar.js', 'src/gfx/pie.js', 'src/gfx/stacked_area.js', 'src/gfx/stacked_bar.js', 'src/gfx/stepup_bar.js',  'src/gfx/waterfall.js', 'src/gfx/table.js', 'src/gfx/tablegraph.js'],
+        src: ['src/module/module_begin.js','src/gfx/graph.js', 'src/util/utility.js', 'src/util/config.js', 'src/util/constants.js', 'src/util/register.js', 'src/util/effects.js', 'src/util/palette.js', 'src/gfx/area.js', 'src/gfx/bar.js', 'src/gfx/donut.js', 'src/gfx/line.js', 'src/gfx/percent_area.js', 'src/gfx/percent_bar.js', 'src/gfx/pie.js', 'src/gfx/polar_area.js', 'src/gfx/stacked_area.js', 'src/gfx/stacked_bar.js', 'src/gfx/stepup_bar.js',  'src/gfx/waterfall.js', 'src/gfx/table.js', 'src/gfx/tablegraph.js','src/module/module_end.js'],
         dest: 'build/uvcharts.js'
       },
 
-      style : {
-        src : ['src/css/uv.css'],
-        dest : 'build/uv.css'
+      test : {
+        src : ['src/util/test.js'],
+        dest : 'build/lvTest.js'
       }
     },
    
@@ -28,7 +32,48 @@ module.exports = function(grunt) {
         files: ['src/gfx/*.js','src/util/*.js','src/css/*.css'],
         tasks: ['concat'],
         options: {
-        interrupt: true,
+          interrupt: true,
+        }
+      }
+    },
+
+    uglify : {
+      options : {
+        mangle : true,
+		banner: '/*! \n<%= pkg.name %> <%= pkg.version %><%= ban.copyright %> <%= ban.licence %>*/\n'
+      },
+
+      gfx : {
+        files : {
+          'build/uvcharts.min.js' : ['<%= concat.gfx.dest %>']
+        }
+      }
+    },
+
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        eqnull: true,
+        browser: true,
+        camelcase : true,
+        //indent: 2,
+        latedef : true,
+        newcap : true,
+        undef : true,
+        //trailing : true,
+        //unused : true,
+        globals: {
+          d3: true,
+          console: true,
+          saveAs: true
+        },
+      },
+      uses_defaults: ['lib/*.js'],
+      
+      gfx: {
+        files: {
+          src: ['<%= concat.gfx.dest %>']
         }
       }
     },
@@ -36,7 +81,7 @@ module.exports = function(grunt) {
     copy : {
       release : {
         files : [  
-          { expand: 'true', cwd: 'build/', src : ['**'], dest : 'dist/' }
+          { expand: 'true', cwd: 'build/', src : ['uv*'], dest : 'dist/' }
         ]
       }
     }
@@ -46,10 +91,13 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task.
-  grunt.registerTask('default', ['concat']);
-  grunt.registerTask('build', ['build_gfx']);
-  grunt.registerTask('build_gfx', ['concat:gfx']);
+  grunt.registerTask('default', ['build']);
+  grunt.registerTask('build', ['build_gfx', 'build_tst']);
+  grunt.registerTask('build_gfx', ['concat:gfx', 'uglify:gfx', 'jshint:gfx']);
+  grunt.registerTask('build_tst', ['concat:test'])
   grunt.registerTask('release', ['build_gfx','copy:release']);
 };
