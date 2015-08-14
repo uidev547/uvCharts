9989568e9814ecdb534dfddbeae043574614e16d
unknown <pankajkumar005@gmail.com>
2015-08-14 20:06:05 +0530

    Changes for implementing click callback for bar chart and stacked bar chart#

--- src/gfx/bar.js
+++ src/gfx/bar.js
@@ -56,6 +56,11 @@ uv.BarGraph.prototype.drawHorizontalBars = function (idx) {
       .call(uv.util.endAll, function (d,i){
         d3.select(this.parentNode.parentNode).selectAll('rect').on('mouseover', uv.effects.bar.mouseover(self, idx));
         d3.select(this.parentNode.parentNode).selectAll('rect').on('mouseout', uv.effects.bar.mouseout(self, idx));
+        if(typeof self.config.graph.clickCallback === "function") {
+          d3.select(this.parentNode.parentNode).selectAll('rect').on('click', function(){
+              self.config.graph.clickCallback.apply(null, [arguments[0]]);
+          });
+        }
       });
 
   bars.append('text')
@@ -110,6 +115,11 @@ uv.BarGraph.prototype.drawVerticalBars = function (idx) {
         .call(uv.util.endAll, function (d,i){
           d3.select(this.parentNode.parentNode).selectAll('rect').on('mouseover', uv.effects.bar.mouseover(self, idx));
           d3.select(this.parentNode.parentNode).selectAll('rect').on('mouseout', uv.effects.bar.mouseout(self, idx));
+          if(typeof self.config.graph.clickCallback === "function") {
+            d3.select(this.parentNode.parentNode).selectAll('rect').on('click', function(){
+              self.config.graph.clickCallback.apply(null, [arguments[0]]);
+          });
+        }
         });
 
 
