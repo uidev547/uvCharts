ccb2f54354d3380e1599490e0abe43f6f2176134
hashd <gitk@danduprolu.com>
2015-09-21 18:35:02 +0530

    Upgrades test files to ES6 standard

--- test/test.js
+++ test/test.js
@@ -1,10 +1,11 @@
-var chai = require("chai"),
+const chai = require("chai"),
   path = require("path"),
   // uvcharts = require(path.join(__dirname, '../build', 'uvcharts')),
   should = chai.should()
 
-describe("Basic test", function () {
-  it("adds correctly", function () {
+describe("Basic test", () => {
+  it("adds correctly", () => {
     (5 + 1).should.be.equal(6)
   })
 })
+
